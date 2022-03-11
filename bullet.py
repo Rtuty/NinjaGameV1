14f1384f63eb692ab024101fef2207d1e3cd4bc7
John Doe <johndoe@example.com>
2022-03-11 13:58:32 +0300

    Bob

--- bullet.py
+++ bullet.py
@@ -4,17 +4,20 @@ class Bullet(Sprite):
     def __init__(self, ai_settings, screen, ship):
         super(Bullet, self).__init__()
         self.screen = screen
-        self.rect = pygame.Rect(0, 0, ai_settings.bullet_width, ai_settings.bullet_height)
+        self.image = pygame.image.load('pictures/suriken.png')
+        self.rotate_image = self.image
+        self.rect = self.image.get_rect()
         self.rect.centerx = ship.rect.centerx
         self.rect.top = ship.rect.top
         self.y = float(self.rect.y)
-        self.color = ai_settings.bullet_color
         self.speed_factor = ai_settings.bullet_speed_factor
-        self.image = pygame.image.load('pictures/suriken.png')
+        self.angle = 0
 
 
     def update(self):
         self.y -= self.speed_factor
         self.rect.y = self.y
+        self.rotate_image = pygame.transform.rotate(self.image, self.angle)
+        self.angle += 4
     def draw_bullet(self):
-        pygame.draw.rect(self.screen, self.color, self.rect)
+        self.screen.blit(self.rotate_image, self.rect)
